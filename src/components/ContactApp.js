630c3943379373584c024097aef269b5a7023f7c
5751mn <edi.eastfield99@gmail.com>
2022-08-18 23:56:52 +0800

    tambah kontak

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
-import { render } from "@testing-library/react";
+import ContactInput from "./ContactInput";
 
 class ContactApp extends React.Component {
     constructor(props) {
@@ -17,11 +17,30 @@ class ContactApp extends React.Component {
         const contacts = this.state.contacts.filter(contact => contact.id !== id);
         this.setState({ contacts });
     }
+
+    onAddContactHandler({ name, tag }) {
+        this.setState((prevState) => {
+            return {
+                contacts: [
+                    ...prevState.contacts,
+                    {
+                        id: +new Date(),
+                        name,
+                        tag,
+                        Image: '/images/default.jpg'
+                    }
+                ]
+            }
+        });
+    }
     
     render() {
         return (
             <div className="contact-app">
-                <h1>Daftar Kontak</h1>
+                <h1>Aplikasi Kontak</h1>
+                <h2>Tambah Kontak</h2>
+                <ContactInput addContact={this.onAddContactHandler} />
+                <h2>Daftar Kontak</h2>
                 <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
             </div>
         );
